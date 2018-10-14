35826e9fb4782bc8cacb2944ffc48f421b396131
FFGFER <ffgfer@gmail.com>
2018-10-14 20:43:06 +0200

    15

--- src/test.py
+++ src/test.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import unittest
-import videojuegos
-import usuarios
+import funcionalidad
 
 class Test(unittest.TestCase):
 
-    test = videojuegos.Videojuegos()
+    test = funcionalidad.Funcionalidad()
 
     def testVideojuegos(self):
         self.assertEqual(self.test.getVideojuego("cad"),False,"El ID del videojuego debe ser entero")
         self.assertEqual(self.test.getVideojuego(1),str('BattleRoayale'),"El ID del videojuego es correcto y es devuelto")
                     
-    test = usuarios.Usuarios()
-
     def testUusarios(self):
         self.assertEqual(self.test.getUsuario("cad"),False,"El ID del usuario debe ser entero")
         self.assertEqual(self.test.getVideojuego(1),str('Fernando Flores'),"El ID del usuario es correcto y es devuelto")
