ced01085e5e0ba85cbacf0ef05ababd725675ecf
FFGFER <ffgfer@gmail.com>
2018-10-15 07:36:20 +0200

    fdsaf

--- src/test.py
+++ src/test.py
@@ -6,17 +6,13 @@ import videojuegos
 
 class Test(unittest.TestCase):
 
-    test = videojuegos.Videojuegos()
-
     def testVideojuegos(self):
-        self.assertEqual(self.test.getVideojuego("cad"),False,"El ID del videojuego debe ser entero")
-        self.assertEqual(self.test.getVideojuego(1),str('BattleRoyale'),"El ID del videojuego es correcto y es devuelto")
-    
-	test = usuarios.Usuarios()
-	
+        self.assertEqual(self.videojuegos.Videojuegos().getVideojuego("cad"),False,"El ID del videojuego debe ser entero")
+        self.assertEqual(self.videojuegos.Videojuegos().getVideojuego(1),str('BattleRoyale'),"El ID del videojuego es correcto y es devuelto")
+
     def testUusarios(self):
-        self.assertEqual(self.test.getUsuario("cad"),False,"El ID del usuario debe ser entero")
-        self.assertEqual(self.test.getUsuario(1),str('Fernando Flores'),"El ID del usuario es correcto y es devuelto")
+        self.assertEqual(self.usuarios.Usuarios().getUsuario("cad"),False,"El ID del usuario debe ser entero")
+        self.assertEqual(self.usuarios.Usuarios().getUsuario(1),str('Fernando Flores'),"El ID del usuario es correcto y es devuelto")
 
 
 if __name__ == '__main__':
