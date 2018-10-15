873724138911093964df921b215a6c87b757b588
FFGFER <ffgfer@gmail.com>
2018-10-15 07:50:30 +0200

    sadf

--- src/testvg.py
+++ src/testvg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import unittest
+import usuarios
 import videojuegos
 
 class Test(unittest.TestCase):
@@ -10,6 +11,7 @@ class Test(unittest.TestCase):
     def testVideojuegos(self):
         self.assertEqual(self.test.getVideojuego("cad"),False,"El ID del videojuego debe ser entero")
         self.assertEqual(self.test.getVideojuego(1),str('BattleRoyale'),"El ID del videojuego es correcto y es devuelto")
+
 
 if __name__ == '__main__':
     unittest.main()                                                  
