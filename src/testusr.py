d08eff163cb360b45e94c58d91cabba6eb5e010a
FFGFER <ffgfer@gmail.com>
2018-10-15 08:07:43 +0200

    adsfdsf

--- src/testusr.py
+++ src/testusr.py
@@ -8,8 +8,8 @@ class Test(unittest.TestCase):
 	test = usuarios.Usuarios()
 
 	def testUusarios(self):
-		self.assertEqual(self.usuarios.Usuarios().getUsuario("cad"),False,"El ID del usuario debe ser entero")
-		self.assertEqual(self.usuarios.Usuarios().getUsuario(1),str('Fernando Flores'),"El ID del usuario es correcto y es devuelto")
+		self.assertEqual(self.test.getUsuario("cad"),False,"El ID del usuario debe ser entero")
+		self.assertEqual(self.test.getUsuario(1),str('Fernando Flores'),"El ID del usuario es correcto y es devuelto")
 
 if __name__ == '__main__':
     unittest.main()          
